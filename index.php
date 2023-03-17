657e381aef3647aa048bcba72844bd78bd862ca2
kundius <kundius.ruslan@gmail.com>
2023-03-17 16:46:24 +0300

    upd

--- index.php
+++ index.php
@@ -4,20 +4,20 @@
     <?php get_template_part('partials/head'); ?>
   </head>
   <body>
-    <?php get_template_part('partials/header'); ?>
+    <div class="wrapper">
+      <?php get_template_part('partials/header'); ?>
 
-    <main class="main">
       <section class="page-section">
-        <div class="ui-container">
-          <h1 class="page-title"><?php the_title() ?></h1>
+        <div class="container">
+          <h1 class="page-section__title"><?php the_title() ?></h1>
 
-          <div class="page-content content">
+          <div class="page-section__content content">
             <?php the_content() ?>
           </div>
         </div>
       </section>
-    </main>
 
-    <?php get_template_part('partials/footer'); ?>
+      <?php get_template_part('partials/footer'); ?>
+    </div>
   </body>
 </html>
