f5e49f973c4912dbe2713c4da9cd3979d204d7fc
kundius <kundius.ruslan@gmail.com>
2023-03-25 16:07:20 +0300

    deploy

--- partials/header.php
+++ partials/header.php
@@ -1,3 +1,36 @@
+<section class="banner-section">
+  <div class="container">
+    <div class="banner">
+      <button type="button" name="button" class="banner__close"></button>
+      <div class="banner__headline">
+        <div class="banner__date">
+          17 - 19<br />
+          апреля 2023
+        </div>
+        <div class="banner__desc">
+          27-ая Международная выставка<br />
+          транспортно-логистических услуг,<br />
+          складского оборудования и технологий TransRussia.
+        </div>
+        <div class="banner__logos">
+          <div class="banner__logo">
+            <img src="" alt="<?php bloginfo('template_url') ?>/dist/images/skladtech.png">
+          </div>
+          <div class="banner__logo">
+            <img src="" alt="<?php bloginfo('template_url') ?>/dist/images/transrussia.png">
+          </div>
+        </div>
+      </div>
+      <div class="banner__body">
+        <div class="banner__title">
+          Будем рады встрече на выставке <span>TransRussia 2023!</span>
+        </div>
+        <a href="#" class="button-primary">ПОДРОБНЕЕ</a>
+      </div>
+    </div>
+  </div>
+</section>
+
 <section class="header">
   <div class="container header__container">
     <a href="/" class="header__logo">
