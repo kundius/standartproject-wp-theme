ca04bd849d30d8f2321b5bda9b6b39d138d36ec6
Kundius Ruslan <kundius.ruslan@gmail.com>
2023-12-20 15:41:54 +0300

    deploy

--- partials/header.php
+++ partials/header.php
@@ -289,26 +289,28 @@
 
 <section class="header">
   <div class="container header__container">
-    <?php if (is_new_year()): ?>
-    <div class="header_new_year_left_top_2"></div>
-    <div class="header_new_year_right_top_2"></div>
-    <div class="header_new_year_middle_top_1"></div>
-    <?php endif; ?>
-    
-    <a href="/" class="header__logo">
-      <img src="<?php bloginfo('template_url') ?>/dist/images/logo.svg" alt="" />
-    </a>
-    <button class="header__toggle" type="button">
-      <span></span>
-      <span></span>
-      <span></span>
-    </button>
-    <nav class="header__nav">
-      <?php wp_nav_menu([
-        'theme_location' => 'mainmenu',
-        'container' => false
-      ]) ?>
-    </nav>
+    <div class="header__wrapper">
+      <?php if (is_new_year()): ?>
+      <div class="header_new_year_left_top_2"></div>
+      <div class="header_new_year_right_top_2"></div>
+      <div class="header_new_year_middle_top_1"></div>
+      <?php endif; ?>
+      
+      <a href="/" class="header__logo">
+        <img src="<?php bloginfo('template_url') ?>/dist/images/logo.svg" alt="" />
+      </a>
+      <button class="header__toggle" type="button">
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+      <nav class="header__nav">
+        <?php wp_nav_menu([
+          'theme_location' => 'mainmenu',
+          'container' => false
+        ]) ?>
+      </nav>
+    </div>
   </div>
 </section>
 
